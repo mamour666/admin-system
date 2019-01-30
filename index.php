aee46627c9612093e035ad25e570af0ac54b4ab6
mamour666 <42503257+mamour666@users.noreply.github.com>
2019-01-30 10:57:54 +0100

    Update index.php

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">      
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
-    <link rel="icon" type="image/png" href="image/2.1%20favicon.png.png">
+    <link rel="icon" type="image/png" href="image/favicon.png">
 </head>
 <nav class="navbar navbar-default navbar-fixed-top">
             <div class="container">

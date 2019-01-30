289c38b07b651372c70563b1f6e142ccbe69525e
mamour666 <42503257+mamour666@users.noreply.github.com>
2019-01-30 11:02:57 +0100

    Update delete.php

--- delete.php
+++ delete.php
@@ -35,7 +35,7 @@ if(!empty($_POST))
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">      
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
-    <link rel="icon" type="image/png" href="image/2.1%20favicon.png.png">
+    <link rel="icon" type="image/png" href="image/favicon.png">
 </head>
     <body>
         <h1></h1>
